a84fb93d3e7c455156ad5737d1bd0e47ff2fc673
emicif <emanuela.cifolelli@gmail.com>
2022-03-10 16:09:49 +0100

    init

---
+++ jsnack1/script.js
@@ -0,0 +1,6 @@
+console.log ('JS OK')
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome 
+// comunicagli se può partecipare o no alla festa.
